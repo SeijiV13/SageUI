7f0f8bed9554c692b750f30af24585c14bc996b3
Seiji Ralph Villafranca <seijiv13@outlook.com>
2025-09-08 18:39:27 +0200

    foxed export path

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,43 @@
-import { defineConfig } from 'rollup';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy';
+import { readFileSync } from 'fs';
 
-export default defineConfig([
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/index.js',
-        format: 'cjs',
-        exports: 'named'
-      },
-      {
-        file: 'dist/index.esm.js',
-        format: 'esm'
-      }
-    ],
-    plugins: [
-      typescript({
-        typescript: require('typescript'),
-        useTsconfigDeclarationDir: true
-      }),
-      postcss({
-        extract: true,
-        minimize: true,
-        use: ['sass']
-      })
-    ],
-    external: ['tailwindcss']
-  }
-]);
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    postcss({
+      extract: 'sageui.css', // This will output to dist/sageui.css
+      minimize: true,
+    }),
+    typescript({
+      typescript: require('typescript'),
+      clean: true,
+    }),
+    copy({
+      targets: [
+        { src: 'src/styles/sageui.css', dest: 'dist/styles' },
+        { src: 'package.json', dest: 'dist' },
+        { src: 'README.md', dest: 'dist' },
+        { src: 'LICENSE', dest: 'dist' }
+      ]
+    })
+  ],
+};
